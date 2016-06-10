be6b1ab96084b09d60bca4f8ff1acbff94dcd3aa
SergioCaldas <sergiocaldas.91@gmail.com>
2016-06-10 17:31:51 +0100

    Added new results

--- Extra/organize.sh
+++ Extra/organize.sh
@@ -7,6 +7,6 @@ for schedule in static dynamic guided
 do
 for nthreads in 1 2 4 8 16 32 64
 do
-cat *"_"$schedule"_"$nthreads"_"*".csv" | grep "Time: " | sed 's/[^0-9.]*//g' | sort > $schedule"_"$nthreads"_time.csv"
+cat *"_"$schedule"_"$nthreads"_"*".csv" | grep "tempo ->" | sed 's/[^0-9.]*//g' | sort > $schedule"_"$nthreads"_time.csv"
 done
 done
