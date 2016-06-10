1373ec9d3df5e3ac3fa80dda3a72f47415e75278
SergioCaldas <sergiocaldas.91@gmail.com>
2016-06-10 16:08:34 +0100

    Added Scripts

---
+++ Extra/run.sh
@@ -0,0 +1,18 @@
+#!bin/bash
+
+for schedule in static dynamic guided
+do
+export OMP_SCHEDULE="$schedule"
+for nthreads in 1 2 4 8 16 32 64
+do
+for seq in 1 2 3 4 5 6 7 8 9 10
+do
+echo "schedulling setted to $OMP_SCHEDULE"
+echo   "./ex2_v2 $nthreads"
+dtrace -s threaded.d -c "./ex2_v2 $nthreads" >> "DTRACE_"$schedule"_"$nthreads"_"$seq".csv"
+done
+done
+done
+
+mkdir __csv
+mv DTRACE*.csv __csv
